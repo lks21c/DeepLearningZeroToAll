e6790756c1e44b5081023b255187c050fb786a0d
lks21c <lks21c@gmail.com>
2017-04-07 17:27:34 +0900

    add my logi

--- mylogi.py
+++ mylogi.py
@@ -34,18 +34,29 @@ hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
 cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
                        tf.log(1 - hypothesis))
 
+cost_summ = tf.summary.scalar("cost", cost)
+
 train = tf.train.GradientDescentOptimizer(learning_rate=learningRate).minimize(cost)
 
 predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+
+accuracy_summ = tf.summary.scalar("accuracy", accuracy)
 
 # Launch graph
 with tf.Session() as sess:
     # Initialize TensorFlow variables
     sess.run(tf.global_variables_initializer())
 
+    merged_summary = tf.summary.merge_all()
+    writer = tf.summary.FileWriter("./logs/mylogi")
+    writer.add_graph(sess.graph)
+
     for step in range(27000):
-        cost_val, _, accr = sess.run([cost, train, accuracy], feed_dict={X: x_data, Y: y_data})
+        cost_val, _, accr, summary = sess.run([cost, train, accuracy, merged_summary], feed_dict={X: x_data, Y: y_data})
+
+        writer.add_summary(summary, global_step=step)
+
         if step % 200 == 0:
             print(step, cost_val, accr)
 
