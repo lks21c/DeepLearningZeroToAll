8e56195a26d2434b27e9bcea920d531ae02db2f9
lks21c <lks21c@gmail.com>
2017-05-05 17:57:51 +0900

    fix bug

--- predict_churn.py
+++ predict_churn.py
@@ -10,22 +10,41 @@ y_data = trainX[:, [-1]]
 
 numFeatures = x_data.shape[1]
 
+print(trainX.shape)
+print(x_data.shape)
+print(y_data.shape)
+print(numFeatures)
+
+
 numLabels = 1
 
-learningRate = 0.01
+learningRate = 0.1
 
 # placeholders for a tensor that will be always fed.
 X = tf.placeholder(tf.float32, shape=[None, numFeatures])
 Y = tf.placeholder(tf.float32, shape=[None, numLabels])
 
-W = tf.Variable(tf.random_normal([numFeatures, numLabels],
+W = tf.Variable(tf.random_normal([numFeatures, 1000],
                                  name="weights"))
 
 b = tf.Variable(tf.random_normal([1, numLabels],
                                  name="bias"))
 
 # Hypothesis using sigmoid: tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
-hypothesis = tf.sigmoid(tf.matmul(X, W) + b)
+layer1 = tf.sigmoid(tf.matmul(X, W) + b)
+
+W2 = tf.Variable(tf.random_normal([1000, 100]), name='weight2')
+b2 = tf.Variable(tf.random_normal([100]), name='bias2')
+layer2 = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+
+W3 = tf.Variable(tf.random_normal([100, 10]), name='weight3')
+b3 = tf.Variable(tf.random_normal([10]), name='bias3')
+layer3 = tf.sigmoid(tf.matmul(layer2, W3) + b3)
+
+W4 = tf.Variable(tf.random_normal([10, 1]), name='weight4')
+b4 = tf.Variable(tf.random_normal([1]), name='bias4')
+hypothesis = tf.sigmoid(tf.matmul(layer3, W4) + b4)
+
 
 # cost/loss function
 cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
