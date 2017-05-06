a053a9f35afe1fb0f14834ef2a62323f79dc6248
Kwangsik Lee <lks21c@gmail.com>
2017-05-06 12:30:15 +0900

    Refactoring

--- predict_churn.py
+++ predict_churn.py
@@ -3,100 +3,114 @@ import numpy as np
 
 tf.set_random_seed(777)  # for reproducibility
 
+# Set Training Data
 trainX = np.loadtxt('/d_drive/refined_data/logIdCnt/trainUserChurnList.csv', delimiter=',', dtype=np.float32)
-
+# feature : 1st column ~ the column before the last column
 x_data = trainX[:, :-1]
+# y value : the last column
 y_data = trainX[:, [-1]]
 
+# Set numFeatures
 numFeatures = x_data.shape[1]
 
-print(trainX.shape)
-print(x_data.shape)
-print(y_data.shape)
-print(numFeatures)
+# Set Cross Validation Data
+cvX = np.loadtxt('/d_drive/refined_data/logIdCnt/cvUserChurnList.csv', delimiter=',', dtype=np.float32)
+x_cvdata = cvX[:, :-1]
+y_cvdata = cvX[:, [-1]]
 
+# Set Test Data
+testX = np.loadtxt('/d_drive/refined_data/logIdCnt/testUserChurnList.csv', delimiter=',', dtype=np.float32)
+x_testdata = testX[:, :-1]
+y_testdata = testX[:, [-1]]
+
+# 1 because it's binary classification
 numLabels = 1
 
+# Set Learning Rate
 learningRate = 0.01
+
+# Iteration
 iteration = 200000
 
-# placeholders for a tensor that will be always fed.
+# X is feature and Y is the right answer.
 X = tf.placeholder(tf.float32, shape=[None, numFeatures])
 Y = tf.placeholder(tf.float32, shape=[None, numLabels])
 
+# Set Hidden Layer 1
 W = tf.Variable(tf.random_normal([numFeatures, numFeatures],
                                  name="weights"))
-
 b = tf.Variable(tf.random_normal([1, numLabels],
                                  name="bias"))
-
-# Hypothesis using sigmoid: tf.div(1., 1. + tf.exp(tf.matmul(X, W)))
 layer1 = tf.sigmoid(tf.matmul(X, W) + b)
 
+# Set Hidden Layer 2
 W2 = tf.Variable(tf.random_normal([numFeatures, numFeatures]), name='weight2')
 b2 = tf.Variable(tf.random_normal([numFeatures]), name='bias2')
 layer2 = tf.sigmoid(tf.matmul(layer1, W2) + b2)
 
+# Set Hidden Layer 3
 W3 = tf.Variable(tf.random_normal([numFeatures, numFeatures]), name='weight3')
 b3 = tf.Variable(tf.random_normal([numFeatures]), name='bias3')
 layer3 = tf.sigmoid(tf.matmul(layer2, W3) + b3)
 
+# Set Output Layer
 W4 = tf.Variable(tf.random_normal([numFeatures, 1]), name='weight4')
 b4 = tf.Variable(tf.random_normal([1]), name='bias4')
 hypothesis = tf.sigmoid(tf.matmul(layer3, W4) + b4)
 
-
-# cost/loss function
+# Set Cost Function as Cross Entropy.
 cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
                        tf.log(1 - hypothesis))
-
+# cost tensor for tensorboard.
 cost_summ = tf.summary.scalar("cost", cost)
 
+# Set default optimizer as GradientDescentOptimizer.
 train = tf.train.GradientDescentOptimizer(learning_rate=learningRate).minimize(cost)
 
+# assums predicted if h(x) exceeds 0.5
 predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
-accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
 
+# accuaracy for training dataset
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+# accuracy for cv dataset
+cv_accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+# accuracy for test dataset
+test_accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+# accracy tensor for tensorboard.
 accuracy_summ = tf.summary.scalar("accuracy", accuracy)
 
-# Add ops to save and restore all the variables.
+# Set saver tensor for saving the model
 saver = tf.train.Saver()
 
-cvX = np.loadtxt('/d_drive/refined_data/logIdCnt/cvUserChurnList.csv', delimiter=',', dtype=np.float32)
-
-x_cvdata = cvX[:, :-1]
-y_cvdata = cvX[:, [-1]]
-
-cv_accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
-
-testX = np.loadtxt('/d_drive/refined_data/logIdCnt/testUserChurnList.csv', delimiter=',', dtype=np.float32)
-
-x_testdata = testX[:, :-1]
-y_testdata = testX[:, [-1]]
-
-test_accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
-
-# Launch graph
+# Run Graph
 with tf.Session() as sess:
-    # Initialize TensorFlow variables
+    # Initialize tensorboard variables
     sess.run(tf.global_variables_initializer())
 
+    # Merge tensorboard summary
     merged_summary = tf.summary.merge_all()
-    writer = tf.summary.FileWriter("./logs/mylogi")
+    # Set tensorboard log path
+    writer = tf.summary.FileWriter("./logs/predictChurn")
+    # Write graph info to tensorboard.
     writer.add_graph(sess.graph)
 
+    # Traing the model until interation ends.
     for step in range(iteration):
+        # train is run wtih x_data, y_data.
         cost_val, _, accr, summary = sess.run([cost, train, accuracy, merged_summary], feed_dict={X: x_data, Y: y_data})
 
+        # Save tensorboard summary while training in in progress.
         writer.add_summary(summary, global_step=step)
 
+        # pring debug info by every 200 step.
         if step % 200 == 0:
             print(step, cost_val, accr)
 
+    # Save ML Model after training.
     save_path = saver.save(sess, "/d_drive/model/model.ckpt")
     print("Model saved in file: %s" % save_path)
 
-    # Accuracy report
+    # Accuracy Report
     h, c, a = sess.run([hypothesis, predicted, accuracy],
                        feed_dict={X: x_data, Y: y_data})
     print("Trains Set Accuracy: ", a)
